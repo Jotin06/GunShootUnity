0e41b52c295b873c9b73094070ac7e4602fd3502
Jotin06 <67071887+Jotin06@users.noreply.github.com>
2020-07-17 18:15:28 -0500

    Update BulletCollisions.cs

--- Unity Tutorials/Assets/Scripts/BulletCollisions.cs
+++ Unity Tutorials/Assets/Scripts/BulletCollisions.cs
@@ -1,9 +1,26 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class BulletCollisions : MonoBehaviour
 {
-		
+
+	void Start()
+	{
+
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
 	
+
+	}
+	private void OnCollisionEnter(Collision collision)
+	{
+		if(collision.gameObject.tag == "Enemy")
+		{
+			Destroy(collision.gameObject);
+		}
+	}
 }

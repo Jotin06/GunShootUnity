b08f0101e2c91ca13e68c7593c64f4f7be617f37
Jotin06 <67071887+Jotin06@users.noreply.github.com>
2020-07-17 16:53:01 -0500

    Unity Tutorial
    All files of GunMechanics  Tutorial

---
+++ Unity Tutorials/Assets/Scripts/GunMechanics.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunMechanics : MonoBehaviour
+{
+	public Transform Bullet;
+	public Transform BulletHolder;
+	public float BulletSpeed;
+	// Start is called before the first frame update
+	void Start()
+	{
+
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		if (Input.GetMouseButton(0))
+		{
+			Transform BulletTrans = Instantiate(Bullet, BulletHolder.position, BulletHolder.rotation);
+			Rigidbody BulletRB = BulletTrans.GetComponent<Rigidbody>();
+			BulletRB.AddRelativeForce(Vector3.forward * BulletSpeed);
+		}
+
+	}
+}
